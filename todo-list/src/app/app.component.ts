c63bcdd0459819d48d8f7e1f8043d1357b81b87a
Amele <amelework.getnet21@gmail.com>
2026-09-14 23:20:07 -0500

    Todo functionality and style added

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface TodoItems 
+{
+    name:string;
+    completed:boolean;
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-list';
+itemControl = new FormControl('');
+items:TodoItems[] = [];
+
+
+
+addItem():void
+{
+    const value= this.itemControl.value?.trim();
+      if(!value)
+      {
+        return;  
+      }
+
+    this.items.push({
+      name:value,
+      completed:false
+    });
+
+  this.itemControl.reset();
+}
+
+toggleItem(item:TodoItems):void
+{
+    item.completed = !item.completed;
+}
+
+get remainingCount(): number
+{
+    return this.items.filter(item => !item.completed).length;
+}
+
 }
